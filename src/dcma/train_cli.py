51a442c9e078c1abcc5ddaa932159666a357eb4e
linus <lagbleze@st.ug.edu.gh>
2025-05-17 00:27:09 +0200

    feat: add train_cli.py

---
+++ src/dcma/train_cli.py
@@ -0,0 +1,30 @@
+from train import ModelTrainer
+import argparse
+from utils import read_minio_data
+import pandas as pd
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Run Model training")
+    parser.add_argument("--model_registry", type=str, required=True)
+    parser.add_argument("--read_data_from_minio",
+                        type=bool
+                        )
+    parser.add_argument("--model_type", required=True, type=str)
+    parser.add_argument("--scoring", required=True, nargs="+")
+    parser.add_argument("--evaluation_metric", required=True)
+    parser.add_argument("--save_model_as", required=True)
+    parser.add_argument("--cv", default=20)
+    parser.add_argument("--model_result_metrics",
+                        nargs="+"
+                        )
+    
+    
+
+
+
+
+
+
+
+
