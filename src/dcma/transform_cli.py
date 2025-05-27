a957a13f8b5081eea11d89c66eb7c0fb848cf3dc
linus <lagbleze@st.ug.edu.gh>
2025-05-27 16:47:58 +0200

    feat: add commandline arguments to transform_cli.py

--- src/dcma/transform_cli.py
+++ src/dcma/transform_cli.py
@@ -13,6 +13,26 @@ def main():
     parser.add_argument("--train_data_path", required=True)
     parser.add_argument("--test_data_path", required=True)
     parser.add_argument("--predictors", required=True)
+    parser.add_argument("--upload_output_to_minio", action="store_true")
+    parser.add_argument("--access_key_env_name", default="MINIO_ACCESS_KEY", help="Env var name for MinIO access key")
+    parser.add_argument("--access_secret_env_name", default="MINIO_SECRET_KEY", help="Env var name for MinIO secret key")
+    parser.add_argument("--minio_server_url_env_name", default="MINIO_SERVER_URL", help="Env var name for MinIO server URL")
+    parser.add_argument("--minio_endpoint_is_secured", action="store_true",
+                        help="Whether the Minio endpoint url is secured"
+                        )
+    parser.add_argument("--local_train_data_path", type=str)
+    parser.add_argument("--local_test_data_path", type=str)
+    parser.add_argument("--train_preprocessed_metadata_filepath", type=str)
+    parser.add_argument("--test_preprocessed_metadata_filepath", type=str)
+    parser.add_argument("--include_sample_weight", action="store_true",
+                        help="Whether to include sample weight in the training"
+                        )
+    parser.add_argument("--mlflow_tracking_uri", type=str, required=True,
+                        help="MLflow tracking URI"
+                        )
+    parser.add_argument("--predictor_variables", nargs="+", required=True,
+                        help="Predictor variable names"
+                        )
     
     
     args = parser.parse_args()
